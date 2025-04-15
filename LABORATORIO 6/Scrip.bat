69ba7a1e994615489a5bd9693a3ac68721abe3d1
Angy-99889 <mamaniaycaangela@gmail.com>
2025-04-14 22:08:00 -0400

    Laboratorio-TEW_300
    Laboratorio 6 la verdad no se si esta bien pero espero que si

---
+++ LABORATORIO 6/Scrip.bat
@@ -0,0 +1,29 @@
+MD "LOGIN CSS"
+CD "LOGIN CSS"
+
+for /L %%i in (1,1,5) do (
+type nul > Ejemplo_%%i.html
+)
+for /L %%i in (1,1,5) do (
+type nul > estilos_%%i.css
+)
+MD..
+MD "REGISTRO DE DATO CSS"
+CD "REGISTRO DE DATO CSS"
+
+for /L %%i in (1,1,5) do (
+type nul > Ejemplo_%%i.html
+)
+for /L %%i in (1,1,5) do (
+type nul > estilos_%%i.css
+)
+MD..
+MD "PORTAFOLIOS CSS"
+CD "PORTAFOLIOS CSS"
+
+for /L %%i in (1,1,5) do (
+type nul > Ejemplo_%%i.html
+)
+for /L %%i in (1,1,5) do (
+type nul > estilos_%%i.css
+)
